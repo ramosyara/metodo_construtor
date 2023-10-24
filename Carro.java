0dfce039adb3dcea5ec20289e3a1ca38135619a2
ramosyara <yara.fernanda.ptal@gmail.com>
2023-10-24 14:04:36 +0000

    salvando

---
+++ Carro.java
@@ -0,0 +1,24 @@
+public class Carro{
+    private String cor;
+    private String modelo;
+    private int aro;
+    private double valorCarro; 
+
+    public Carro(){
+
+
+    }
+    public Carro(int aro){
+        this.aro=aro;
+    }
+    public Carro(int aro, String modelo){
+        this.aro=aro;
+        this.modelo=modelo;
+    }
+    public Carro(int aro, String modelo, String cor, double valorCarro){
+        this.aro=aro;
+        this.modelo=modelo;
+        this.valorCarro=valorCarro;
+        this.cor=cor;
+    }
+}
